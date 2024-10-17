6c3db97a2240350e2fd648713fd8247c6338686f
Steffen Schulze <steffen.schulze@t-systems.com>
2024-10-17 07:38:49 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: "https://eclipse-xfsc.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/org.eclipse.xfsc",
+  baseUrl: "/landingpage",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
